341fe2b8efe6ecbf14d8aa52c115abed54c01df4
Pedro Paulo <pedropaulo.developer@gmail.com>
2023-03-30 23:27:38 -0300

    ci: :wrench: configurated jest and testing library

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ export default {
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   transform: {
-    ".+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.tsx?$": "ts-jest",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.cjs',
   },
 };
